4bc42fbd04a2d74be506edeb6f89b2efb29c00f3
itzhak12380 <iascc12380@gmail.com>
2021-12-22 15:38:54 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const Express = require('express')
+const cors = require('cors')
+const app = Express()
+app.use(cors())
+app.use(Express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.json({message:"hey"})
+})
+
+
+app.listen(8080,(error)=>{
+    if(error) throw error
+    console.log("server is live");
+})
